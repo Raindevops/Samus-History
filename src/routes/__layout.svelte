8d61bf8d3f4e341689f6920826136a038eb032e3
raindevops <raindevops.durand@gmail.com>
2021-11-21 10:05:42 +0100

    add layout page & error page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a class="white" href="#biography">Biographie</a></li>
+        <li><a class="white" href="#personnality">Personnalité</a></li>
+        <li><a class="white" href="#skills">Aptitudes et compétences</a></li>
+    </ul>
+</nav>
+
+<slot></slot>
